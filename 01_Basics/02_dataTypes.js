2f0f55670183359bd59c8381d570b60c6a3f0c8e
Soubhik <soubhikpramanik10@gmail.com>
2024-09-19 12:37:30 +0530

    Add Datatype and ECMA standards

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //Treat all JS code as newer version
+
+// alert(3+3)  // We are Using nodejs, not browser
+
+console.log(3
+     + 
+      3); // code readability should be high
+
+console.log("Soubhik");
+
+
+let name = "Soubhik";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to the power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); // Result ==> Object
+console.log(typeof undefined); // Result ==> Undefined
