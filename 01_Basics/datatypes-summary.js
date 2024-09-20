d1ee6333a4f77171194362479815559507c083cd
Soubhik <soubhikpramanik10@gmail.com>
2024-09-20 13:27:23 +0530

    Add Data types of JavaScript Summary file

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,48 @@
+// Primitive 
+// ||
+// 7 Type : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+//console.log(typeof score); // ===> Number
+//console.log(typeof scoreValue); // ===> Number
+//console.log(typeof isLoggedIn); // ===> Boolean
+//console.log(typeof outsideTemp); // ===> Object
+//console.log(typeof userEmail);  // ===> Undefined
+
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+// console.log(id === anotherId); //===> False
+
+// const bigNumber = 4353562625446473373563553n
+
+// ========================================================= //
+
+// Reference (Non Primitive)
+// Array, Objects, Functions
+
+//Array
+const hero = ["Shaktiman" , "Naagraj" , "Doga"];
+
+//Object
+let myObj = {
+    name : "Soubhik",
+    age : 22,
+}
+
+//Function
+const myFunction = function(){
+    console.log("Hello World !");
+    
+}
+
+//console.log(typeof myFunction); // ===> Object Function
+//console.log(typeof myObj); // ===> Object
+//console.log(typeof hero);  // ===> Object
