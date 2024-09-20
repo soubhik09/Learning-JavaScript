9eabbc7d6473785633b77830f996463cf035a901
Soubhik <soubhikpramanik10@gmail.com>
2024-09-20 12:14:16 +0530

    Add Datatype conversion confusion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = "33abc";
+let example = null;
+let example2 = undefined;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+let valueInNum = Number(example)
+let valueInNum2 = Number(example)
+
+// console.log(typeof valueInNumber);
+// console.log(typeof valueInNum);
+// console.log(typeof valueInNum2);
+
+// console.log(valueInNumber);
+// console.log(valueInNum);
+// console.log(valueInNum2);
+
+
+//"33"  => 33
+// "33qbc"  => NaN
+// true  => 1; false  => 0;
+
+// let isLoggedIn = 1;
+// let isLoggedIn = "";
+// let isLoggedIn = "Soubhik";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Soubhik" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
