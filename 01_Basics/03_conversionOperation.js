2ea26be09a790656affc612fa488623c378a088a
Soubhik <soubhikpramanik10@gmail.com>
2024-09-20 12:38:47 +0530

    Add Why string to number conversion is confusing

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -37,4 +37,52 @@ let valueInNum2 = Number(example)
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************************** Operations ***********************//
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); ====> 2 to the power 3
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Soubhik";
+
+let str3 = str1 + str2;
+
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
