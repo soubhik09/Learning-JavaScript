b77600555cc0e8a6a81252ef58b9a68d1ddefc18
Soubhik <soubhikpramanik10@gmail.com>
2024-09-21 13:45:35 +0530

    Add String in JavaScript

---
+++ 01_Basics/05_String.js
@@ -0,0 +1,41 @@
+const name = "Soubhik-pra"
+const repoCount = 50
+
+// String Interpolation-----
+console.log(`Hello my name is ${name} and my repo is ${repoCount}`);
+
+// String Declearation
+const gameName = new String("Soubhikpr")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('p'));
+
+
+// const newString = gameName.substring(5,8)
+// console.log(newString);
+
+
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
+
+const newStringOne = "    Soubhik    "
+console.log(newStringOne);
+
+console.log(newStringOne.trim());
+
+const url = "https://soubhik.com/soubhik%20pramanik"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes("soubhik"));
+
+const myname = "--s--o-u--b--h---i--k"
+
+console.log(myname.split('-'));
+
